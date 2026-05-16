e81155373eb1e1d4243f5a964a12ffde59131636
harekuiin <ara.zenteno@duocuc.cl>
2026-05-15 20:00:23 -0400

    feature(frontend): agregar configuraciones de Vite y entradas HTML

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/app.css'
+import UsuarioCard from './components/UsuarioCard'
+
+export default function App(){
+  return (
+    <div className="app">
+      <h1>Frontend - Fase 1</h1>
+      <UsuarioCard />
+    </div>
+  )
+}
